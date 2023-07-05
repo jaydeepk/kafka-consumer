cc8ea5aba2bdec4bbd428905a9611ff25c6dca4c
jaydeep <jaydeepk@gmail.com>
2023-07-05 13:20:34 +0530

    Added complex schema with arrays

--- src/main/kotlin/com/example/kafkaconsumer/controllers/order.kt
+++ src/main/kotlin/com/example/kafkaconsumer/controllers/order.kt
@@ -6,6 +6,7 @@ import org.apache.kafka.clients.producer.ProducerRecord
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RestController
+import java.io.File
 import java.util.*
 
 @RestController
@@ -22,10 +23,36 @@ class order {
         props["key.serializer"] = "org.apache.kafka.common.serialization.StringSerializer"
         props["value.serializer"] = "org.apache.kafka.common.serialization.StringSerializer"
         val producer = KafkaProducer<String, String>(props)
-        producer.send(ProducerRecord<String, String>("test-topic","""{"id": 1, "name" : "John", }"""))
-        producer.send(ProducerRecord<String, String>("test-topic","""{"id": 1, "name" : "Jim", }"""))
-        producer.send(ProducerRecord<String, String>("product-queries","""{"name" : "John", "inventory": 10, "id": 1}"""))
-        producer.send(ProducerRecord<String, String>("product-queries","""{"name" : "Jim", "inventory": 20, "id": 2}"""))
+        sendMessagesToComplexArrayTopic(producer)
         producer.close()
+    }
+
+    private fun sendMessagesToComplexArrayTopic(producer: KafkaProducer<String, String>){
+        val messageValueJson = File("src/test/resources/complex_schema_with_arrays_invalid_example.json").readText()
+        producer.send(
+            ProducerRecord<String, String>(
+                "product-queries",
+                messageValueJson
+            )
+        )
+    }
+    private fun sendMessagesToProductQueries(producer: KafkaProducer<String, String>) {
+        producer.send(
+            ProducerRecord<String, String>(
+                "product-queries",
+                """{"name" : "John", "inventory": 10, "id": 1}"""
+            )
+        )
+        producer.send(
+            ProducerRecord<String, String>(
+                "product-queries",
+                """{"name" : "Jim", "inventory": 20, "id": 2}"""
+            )
+        )
+    }
+
+    private fun sendMessagesToTestTopic(producer: KafkaProducer<String, String>) {
+        producer.send(ProducerRecord<String, String>("test-topic", """{"id": 1, "name" : "John", }"""))
+        producer.send(ProducerRecord<String, String>("test-topic", """{"id": 1, "name" : "Jim", }"""))
     }
 }
